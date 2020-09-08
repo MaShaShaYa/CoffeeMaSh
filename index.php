8fd9bf0ed9ad72bab44d3dc3b5ac3f29913975fd
MaShaShaYa <acppro@yandex>
2020-09-08 11:53:59 +0300

    нарисовали кофе-машина

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 <div class="container">
     <div class="row">
         
-        <div class="col-6 coffee-list">
+        <div class="col-6 coffee-list d-flex flex-column justify-content-around">
             
             <div class="coffee-item">
                 <img src="img/americano.png" alt="Американо">
@@ -42,6 +42,45 @@
         </div>
         
         <div class="col-6 coffee-oper">
+            <div class="row">
+                
+                <div class="col-6">
+                    <div class="display">
+                        <div class="p">Выберите кофе</div>
+                            <div class="progress">
+                              <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%"></div>
+                            </div>
+                        
+                    </div>
+                    
+                    <div class="cup-container">
+                        <img src="img/americano.png" alt="" class="cup">
+                        
+                    </div>
+                    
+                </div>
+                
+                
+                
+                <div class="col-6">
+                    
+                    <div class="input-group mb-3">
+                      <input type="text" class="form-control" placeholder="Баланс">
+                      <div class="input-group-append">
+                        <span class="input-group-text">&#8381;</span>
+                      </div>
+                    </div>
+                    
+                    <div class="atm-container">
+                        <img src="img/bill_acc.png" alt="" class="atm">
+                    </div>
+                    
+                    <button class="btn btn-primary btn-block my-2">Сдача</button>
+                    <div class="change-container">возьмите ваши денюжки</div>
+                    
+                </div>
+                
+            </div>
         </div>
         
     </div>
